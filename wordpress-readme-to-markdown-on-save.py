2d879880d3a36d339c6410f8ac54103bced2348c
Mário Valney <mariovalney@gmail.com>
2020-02-07 09:53:24 -0300

    added a alert about changing readme.md directly

--- wordpress-readme-to-markdown-on-save.py
+++ wordpress-readme-to-markdown-on-save.py
@@ -1,17 +1,25 @@
 import sublime
 import sublime_plugin
-import re
+import os.path
 
 class WordpressReadmeToMarkdownOnSave(sublime_plugin.EventListener):
 
-    def __init__(self):
-        self.plugin_readme = re.compile("""(.+)/readme.txt$""")
-
     def on_post_save(self, view):
-        name = view.file_name()
+        if self.get_settings(view).get('on-save') == False:
+            return
 
-        if self.get_settings(view).get('on-save') and self.plugin_readme.search(name):
-            view.window().run_command('wordpress_readme_to_markdown')
+        file = view.file_name()
+
+        if 'README.md' == file[-9:]:
+            file_txt = file.replace("README.md", "readme.txt")
+
+            if os.path.isfile(file_txt):
+                sublime.error_message("You are saving a README.md file directly, but autosave is ON. \nYour changes will be lost if you save readme.txt.")
+                return
+
+            return
+
+        view.window().run_command('wordpress_readme_to_markdown')
 
     @staticmethod
     def get_settings(view):
